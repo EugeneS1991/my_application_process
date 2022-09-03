de92135183902aad73641d0566f4a9c9551c25fd
EugeneS1991 <54810605+EugeneS1991@users.noreply.github.com>
2022-09-03 23:43:00 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ import json
 import time
 from google.cloud import bigquery
 from flask import Flask, request, Response, g
-
+import logging
 app = Flask(__name__)
-
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 @app.route('/process',methods=['GET', 'POST'])
 def log():
 
@@ -12,9 +13,9 @@ def log():
 
     payload = raw_req_data.get_data(as_text=True) or '(empty payload)'
     print('Received task with payload: {}'.format(payload))
-
+    app.logger.info(payload)
     return 'Received task with payload: {}'.format(payload)
 
 
 if __name__ == '__main__':
-    app.run(debug=False, port=80, host='0.0.0.0')
+app.run(host='0.0.0.0', port=8080, threaded=True, debug=True)
